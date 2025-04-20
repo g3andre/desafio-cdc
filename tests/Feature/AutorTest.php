5a8ab98beab31a3b323161f78f9008e734e0bad2
g3andre <andre.mmp@gmail.com>
2025-04-20 08:06:03 -0300

    test: Adiciona testes para validação de dados do autor e ajusta endpoint para '/api/autores'

--- tests/Feature/AutorTest.php
+++ tests/Feature/AutorTest.php
@@ -2,9 +2,9 @@
 
 namespace Tests\Feature;
 
-use App\Models\Autor;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\TestDox;
 use Tests\TestCase;
@@ -23,7 +23,7 @@ class AutorTest extends TestCase
             'descricao' => 'Descrição do autor teste',
         ];
 
-        $response = $this->postJson('/api/autor', $autor);
+        $response = $this->postJson('/api/autores', $autor);
 
         $response->assertStatus(200);
         $response->assertContent('');
@@ -32,5 +32,73 @@ class AutorTest extends TestCase
             'email' => $autor['email'],
             'descricao' => $autor['descricao'],
         ]);
+    }
+
+    public static function autorInvalidoDataProvider(): array
+    {
+        return [
+            'Nome vazio' => [
+                [
+                    'nome' => '',
+                    'email' => 'autor@teste.com',
+                    'descricao' => 'Descrição do autor teste',
+                ],
+                [
+                    'nome' => ['The nome field is required.'],
+                ]
+            ],
+
+            'Email vazio' => [
+                [
+                    'nome' => 'Autor Teste',
+                    'email' => '',
+                    'descricao' => 'Descrição do autor teste',
+                ],
+                [
+                    'email' => ['The email field is required.'],
+                ]
+            ],
+            'Email inválido' => [
+                [
+                    'nome' => 'Autor Teste',
+                    'email' => 'email-invalido',
+                    'descricao' => 'Descrição do autor teste',
+                ],
+                [
+                    'email' => ['The email field must be a valid email address.'],
+                ]
+            ],
+            'Descricao vazia' => [
+                [
+                    'nome' => 'Autor Teste',
+                    'email' => 'autor@teste.com',
+                    'descricao' => '',
+                ],
+                [
+                    'descricao' => ['The descricao field is required.'],
+                ]
+            ],
+            'Descricao acima de 400 caracteres' => [
+                [
+                    'nome' => 'Autor Teste',
+                    'email' => 'autor@teste.com',
+                    'descricao' => Str::random(401),
+                ],
+                [
+                    'descricao' => ['The descricao field must not be greater than 400 characters.'],
+                ]
+            ]
+        ];
+    }
+
+    #[Test]
+    #[TestDox('Testa se o autor não é criado com dados inválidos')]
+    #[DataProvider('autorInvalidoDataProvider')]
+    public function naoCriaAutorComDadosInvalidos(array $payload, array $erros): void
+    {
+        $response = $this->postJson('/api/autores', $payload);
+
+        $response->assertStatus(422);
+        $response->assertInvalid($erros);
     }
 }
